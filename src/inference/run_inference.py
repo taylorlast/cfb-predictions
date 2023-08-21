ff03d0c0740c7e31ee250e0cf27157becd6030fb
taylorlast <taylorlast@gmail.com>
2023-08-20 23:07:11 -0400

    add inference function

---
+++ src/inference/run_inference.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+import cfbd
+
+import requests
+import json
+import os
+import sys
+
+from datetime import datetime as dt
+
+from preprocessing.preprocessing import get_latest_feature_values
+
+
+def run_inference():
+    pass
+
